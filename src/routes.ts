550d3a77d2ee58366fe21e379efaac35c4ca6b6c
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-09-17 19:22:42 -0300

    register user

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import { Request, Response, Router } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ ok: true });
-});
+router.post('/users', new CreateUserController().handle);
 
 export { router };
