5db6751c4cc3510c10fb4d9a3a0aea4174c7bfa5
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-09-18 10:24:46 -0300

    feat: expose static image url

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import path from 'path';
 
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
 app.use(cors());
+app.use(router);
+
+// rota estatica para expor pasta de imagens
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
